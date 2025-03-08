a6893de6f6f38f4c8798a2dbf86156008eaa1591
pragyanaischool <75975970+pragyanaischool@users.noreply.github.com>
2025-03-08 11:57:08 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from langchain import OpenAI
+from langchain.chat_models import ChatGroq
 from langchain.docstore.document import Document
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.chains.summarize import load_summarize_chain
@@ -7,10 +7,15 @@ from langchain.chains.summarize import load_summarize_chain
 # Page title
 st.set_page_config(page_title='🦜🔗 Text Summarization App')
 st.title('🦜🔗 Text Summarization App')
-
+client = Groq(
+    api_key=st.secrets["GROQ_API_KEY"],
+)
 def generate_response(txt):
     # Instantiate the LLM model
-    llm = OpenAI(temperature=0, openai_api_key=openai_api_key)
+    #llm = ChatGroq(model_name="llama3-8b-8192", temperature=0, groq_api_key=groq_api_key)
+
+    #llm = OpenAI(temperature=0, openai_api_key=openai_api_key)
+    llm = ChatGroq(model_name="llama3-8b-8192", temperature=0, groq_api_key=st.secrets["GROQ_API_KEY"])
     # Split text
     text_splitter = CharacterTextSplitter()
     texts = text_splitter.split_text(txt)
