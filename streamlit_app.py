1e5a0b9f334dea938a744952c48dceeed85b5e7d
pragyanaischool <75975970+pragyanaischool@users.noreply.github.com>
2025-03-08 11:58:25 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,13 +32,13 @@ txt_input = st.text_area('Enter your text', '', height=200)
 # Form to accept user's text input for summarization
 result = []
 with st.form('summarize_form', clear_on_submit=True):
-    openai_api_key = st.text_input('OpenAI API Key', type = 'password', disabled=not txt_input)
-    submitted = st.form_submit_button('Submit')
-    if submitted and openai_api_key.startswith('sk-'):
+    #openai_api_key = st.text_input('OpenAI API Key', type = 'password', disabled=not txt_input)
+    #submitted = st.form_submit_button('Submit')
+    #if submitted and openai_api_key.startswith('sk-'):
         with st.spinner('Calculating...'):
             response = generate_response(txt_input)
             result.append(response)
-            del openai_api_key
+           # del openai_api_key
 
 if len(result):
     st.info(response)
