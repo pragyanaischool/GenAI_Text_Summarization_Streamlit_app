f52c01843973a9ab4a291146d8bba79f037b6dce
pragyanaischool <75975970+pragyanaischool@users.noreply.github.com>
2025-03-08 12:25:41 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,8 @@
 import streamlit as st
 from groq import Groq
 #from langchain.chat_models import ChatGroq
-from langchain.chat_models import ChatOpenAI
+from langchain_groq import ChatGroq
+#from langchain.chat_models import ChatOpenAI
 from langchain.docstore.document import Document
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.chains.summarize import load_summarize_chain
@@ -9,21 +10,21 @@ from langchain.chains.summarize import load_summarize_chain
 # Page title
 st.set_page_config(page_title='🦜🔗 Text Summarization App')
 st.title('🦜🔗 Text Summarization App')
-#client = Groq(
-#    api_key=st.secrets["GROQ_API_KEY"],
-#)
+client = Groq(
+    api_key=st.secrets["GROQ_API_KEY"],
+)
 def generate_response(txt):
     # Instantiate the LLM model
     #llm = ChatGroq(model_name="llama3-8b-8192", temperature=0, groq_api_key=groq_api_key)
 
     #llm = OpenAI(temperature=0, openai_api_key=openai_api_key)
-    #llm = ChatGroq(model_name="llama3-8b-8192", temperature=0, groq_api_key=st.secrets["GROQ_API_KEY"])
+    llm = ChatGroq(model_name="llama3-8b-8192", temperature=0, groq_api_key=st.secrets["GROQ_API_KEY"])
     # Instantiate the LLM model with Groq API
-    llm = ChatOpenAI(
-        model_name="llama3-8b-8192",
-        temperature=0,
-        openai_api_key=st.secrets["GROQ_API_KEY"],
-        openai_api_base="https://api.groq.com/openai/v1")
+    #llm = ChatOpenAI(
+    #    model_name="llama3-8b-8192",
+    #    temperature=0,
+    #    openai_api_key=st.secrets["GROQ_API_KEY"],
+    #    openai_api_base="https://api.groq.com/openai/v1")
     # Split text
     text_splitter = CharacterTextSplitter()
     texts = text_splitter.split_text(txt)
